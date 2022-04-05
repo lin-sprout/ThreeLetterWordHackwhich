e89f4661cae467cafa8d32e7c556a7eb65dd997b
Lin LaMonte <penelopelamonte@gmail.com>
2022-04-05 14:10:26 -0600

    started adding in button function

--- ThreeLetterWordHackwhich/ViewController.swift
+++ ThreeLetterWordHackwhich/ViewController.swift
@@ -17,18 +17,39 @@ class ViewController: UIViewController
     @IBOutlet weak var stackViewHorz: UIStackView!
     
     
+    var letters = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var counter: Int = 0
+    var currentLetter = "A"
+   
+    
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
     }
 
     
     @IBAction func whenTapped(_ sender: Any)
     {
+
+        currentLetter = letters[counter]
+        currentLetterLabel.text = currentLetter
     
-    
-    
+//can click on item(ex: ? boxes) and tag them, which assings them a # will be useful for next proj//
+        
+        let selectedPoint = ((sender as! AnyObject).location(in:stackViewHorz))
+        
+        for label in wordLabels
+        {
+            if
+                label.frame.contains(selectedPoint)
+            {
+                label.text = "\(currentLetter)"
+            }
+        }
+    counter += 1
     }
     
 }
